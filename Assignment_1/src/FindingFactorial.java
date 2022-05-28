2956869d683d5ef2c61530e6f0033f419ab79d9e
BasantaBabu <yuvasathi@gmail.com>
2022-05-27 23:16:18 -0400

    Assignment-1 question 1 done

---
+++ Assignment_1/src/FindingFactorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FindingFactorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+	       System.out.print("Enter the number here: ");
+	       int number= input.nextInt();
+	 
+	       int factorial = 1;
+	       int i = 1;
+	       while(i<=number)
+	       {
+	           factorial = factorial * i;
+	           i++;
+	       }
+	       System.out.println("Factorial of "+number+" is: "+factorial);
+
+	}
+
+}
